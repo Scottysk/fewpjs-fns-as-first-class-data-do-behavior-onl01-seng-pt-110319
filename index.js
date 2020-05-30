9db02003195b272f36c35f607828fd6cd12a8b49
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-05-30 02:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ function handleClick(e) {
 function greet(time) {
   let hour = time.split(":")[0]
   if (hour < 12) {
-    return "Good Morning"
+    return "Good Morning";
+  }
+  else if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon";
   }
 }
